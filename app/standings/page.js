b11691a835fd3e3d108ce733427ab174adfd31f9
tymull24-max <tymull24@gmail.com>
2026-05-20 23:43:00 -0400

    Create page.js

---
+++ app/standings/page.js
@@ -0,0 +1,9 @@
+import StandingsTable from "../../components/StandingsTable";
+
+export default function StandingsPage() {
+  return (
+    <div className="max-w-3xl mx-auto mt-10">
+      <StandingsTable />
+    </div>
+  );
+}

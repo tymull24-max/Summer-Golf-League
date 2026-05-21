022946b22ca1abebdabc310e564025fce612a0a3
tymull24-max <tymull24@gmail.com>
2026-05-20 23:46:19 -0400

    Create page.js

---
+++ app/leaderboard/page.js
@@ -0,0 +1,9 @@
+import Leaderboard from "../../components/Leaderboard";
+
+export default function LeaderboardPage() {
+  return (
+    <div className="max-w-3xl mx-auto mt-10">
+      <Leaderboard />
+    </div>
+  );
+}
